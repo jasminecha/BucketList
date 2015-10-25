f70fd81ca00247ce045301b2eb5ffe142f20c140
jasminecha <jbc4gy@virginia.edu>
2015-10-25 11:51:11 -0400

    currently adds to the list and adds event to the calendar.

--- GraduateList/ItemDetailViewController.swift
+++ GraduateList/ItemDetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import EventKitUI
 
-class ItemDetailViewController: UIViewController {
+class ItemDetailViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var taskName: UITextField!
     var name: String = ""
@@ -18,9 +18,16 @@ class ItemDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        taskName.delegate = self
         // Do any additional setup after loading the view.
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.add
+        textField.resignFirstResponder()
+        return true
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -28,40 +35,40 @@ class ItemDetailViewController: UIViewController {
     }
         
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        addEvent()
         if segue.identifier == "doneSegue" {
             name = taskName.text!
         }
     }
 
-//    func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
-//        let event = EKEvent(eventStore: eventStore)
-//        
-//        event.title = title
-//        event.startDate = startDate
-//        event.endDate = endDate
-//        event.calendar = eventStore.defaultCalendarForNewEvents
-//        do {
-//            try eventStore.saveEvent(event, span: .ThisEvent)
-//            print(taskName.text)
-//        } catch {
-//            print("Bad things happened")
-//        }
-//    }
-//    
-//    @IBAction func addEvent(sender: UIButton) {
-//        let eventStore = EKEventStore()
-//        
-//        //let startDate = NSDate()
-//        
-//        //let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
-//        
-//        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
-//            eventStore.requestAccessToEntityType(.Event, completion: {
-//                granted, error in
-//                self.createEvent(eventStore, title: (self.taskName?.text)!, startDate: self.startDate.date, endDate: self.endDate.date)
-//            })
-//        } else {
-//            createEvent(eventStore, title: (self.taskName?.text)!, startDate: self.startDate.date, endDate: self.endDate.date)
-//        }
-//    }
+    func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
+        let event = EKEvent(eventStore: eventStore)
+        
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        do {
+            try eventStore.saveEvent(event, span: .ThisEvent)
+        } catch {
+            print("Bad things happened")
+        }
+    }
+    
+    func addEvent() {
+        let eventStore = EKEventStore()
+        
+        //let startDate = NSDate()
+        
+        //let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: {
+                granted, error in
+                self.createEvent(eventStore, title: (self.taskName?.text)!, startDate: self.startDate.date, endDate: self.endDate.date)
+            })
+        } else {
+            createEvent(eventStore, title: (self.taskName?.text)!, startDate: self.startDate.date, endDate: self.endDate.date)
+        }
+    }
 }
